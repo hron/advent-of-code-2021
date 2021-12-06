62f174aa3bc41039848a821c4c1ceea2c1b53822
Aleksei Gusev <aleksei.gusev@gmail.com>
2021-12-06 15:03:43 +0200

    Add support for diagonal lines

--- aoc/day05/line.py
+++ aoc/day05/line.py
@@ -1,3 +1,5 @@
+from itertools import zip_longest
+
 from aoc.day05.point import Point
 
 
@@ -25,10 +27,12 @@ class Line:
         elif self.p1.y == self.p2.y:
             return {Point(x, self.p1.y) for x in range(min(xxs), max(xxs) + 1)}
         else:
-            raise DiagonalLineNotSupported
-
-
-class DiagonalLineNotSupported(RuntimeError):
-    pass
+            step_x = +1 if self.p1.x < self.p2.x else -1
+            step_y = +1 if self.p1.y < self.p2.y else -1
+            return {
+                Point(x, y)
+                for (x, y) in zip_longest(range(self.p1.x, self.p2.x + step_x, step_x),
+                                          range(self.p1.y, self.p2.y + step_y, step_y))
+            }
 
 
--- tests/test_day05.py
+++ tests/test_day05.py
@@ -27,12 +27,18 @@ def test_part1():
 #
 
 def test_part2():
-    assert part2.result(None) == None
+    assert part2.result(sample_input) == 12
 
 
 def test_line_points():
     line = Line(Point(0, 9), Point(5, 9))
     assert line.points() == {Point(i, 9) for i in range(0, 6)}
+
+    diagonal_line = Line(Point(1, 1), Point(3, 3))
+    assert diagonal_line.points() == {Point(x, y) for (x, y) in [(1,1), (2,2), (3,3)]}
+
+    diagonal_line2 = Line(Point(9, 7), Point(7, 9))
+    assert diagonal_line2.points() == {Point(x, y) for (x, y) in [(9,7), (8,8), (7,9)]}
 
 
 def test_line_intersection_points():
